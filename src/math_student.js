5fc9ea8dece7587dc91067ae5483cd03aadad72b
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-03-22 23:53:09 +0530

    tests for raw commands

--- src/math_student.js
+++ src/math_student.js
@@ -25,18 +25,15 @@ const test = async (command, args, expected) => {
   assertEquals(JSON.parse(response), expected);
 };
 
-const testRaw = async (command, expected) => {
-  await connection.write(encode(command));
+const testRaw = async (message, expected) => {
+  const [command, ...args] = message.split(/\s+/);
 
-  const response = new Uint8Array(1024);
-  const byteCount = await connection.read(response);
-  const actual = decode(response.subarray(0, byteCount));
-
-  assertEquals(JSON.parse(actual), expected);
+  await test(command, args.map(Number), { result: expected });
 };
 
 const runAllRawTests = async () => {
   await testRaw("ADD 1 2", 3);
+  await testRaw("ADD 1  2", 3);
 };
 
 const runAllTests = async () => {
@@ -47,5 +44,5 @@ const runAllTests = async () => {
 const port = parseInt(Deno.args.at(0));
 const connection = await Deno.connect({ port });
 
-await runAllTests();
+await runAllRawTests();
 console.log("All tests passed!!!");
