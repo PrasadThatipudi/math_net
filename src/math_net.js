b1a3e16a6f9b13afd255fcb874eb6b760a5c5075
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-03-22 21:31:35 +0530

    abstracts server code into functions

--- src/math_net.js
+++ src/math_net.js
@@ -1,12 +1,29 @@
 const connectionAddress = (addr) => `${addr.hostname}:${addr.port}`;
 
-const port = parseInt(Deno.args.at(0));
-const listener = Deno.listen({ port });
-
-for await (const connection of listener) {
+const displayConnectionSuccessMsg = (connection) => {
   const message = `Client connected from ${connectionAddress(
     connection.remoteAddr
   )}`;
 
   console.log(message);
-}
+};
+
+const handleConnection = async (connection) => {
+  const reader = connection.readable.getReader();
+  const request = await reader.read();
+
+  await connection.write(request.value);
+};
+
+const startServer = async (port) => {
+  const listener = Deno.listen({ port });
+
+  for await (const connection of listener) {
+    displayConnectionSuccessMsg(connection);
+    await handleConnection(connection);
+  }
+};
+
+const port = parseInt(Deno.args.at(0));
+
+await startServer(port);
--- src/math_student.js
+++ src/math_student.js
@@ -1,2 +1,9 @@
 const port = parseInt(Deno.args.at(0));
- await Deno.connect({ port });
+const connection = await Deno.connect({ port });
+
+await connection.write(new TextEncoder().encode("hello"));
+
+const reader = connection.readable.getReader();
+const response = await reader.read();
+
+console.log(new TextDecoder().decode(response.value));
