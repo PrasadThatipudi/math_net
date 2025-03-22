a0e060b1f079a5dd86b4fc52644c834af3815d6b
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-03-22 20:32:07 +0530

    server accepts multiple clients

--- src/math_net.js
+++ src/math_net.js
@@ -1,2 +1,12 @@
+const connectionAddress = (addr) => `${addr.hostname}:${addr.port}`;
+
 const port = parseInt(Deno.args.at(0));
-Deno.listen({ port });
+const listener = Deno.listen({ port });
+
+for await (const connection of listener) {
+  const message = `Client connected from ${connectionAddress(
+    connection.remoteAddr
+  )}`;
+
+  console.log(message);
+}
